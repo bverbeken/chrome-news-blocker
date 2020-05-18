ef8f8d755df716437cdd8b0750e1fdf79abe537a
Ben Verbeken <ben@seats.io>
2020-05-18 11:07:20 +0200

    added sporza.be to the list

--- background.js
+++ background.js
@@ -3,7 +3,8 @@ const urls = [
 	"vrt.be/vrtnws",
 	"demorgen.be",
 	"standaard.be",
-	"hln.be"
+	"hln.be",
+	"sporza.be"
 ].map(s => "*://*." + s + "/*");
 
 chrome.webRequest.onBeforeRequest.addListener(
